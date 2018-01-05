20592305ddff0ad4272cb1894f2664765576192c
miaoyuxin <1151815317@qq.com>
2018-01-05 20:08:50 +0800

    rank page layout

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -1,16 +1,114 @@
 <template>
-  <div>
-    排行页面
+  <div class="rank" ref="rank">
+    <scroll
+      :data="topList"
+      ref="topList"
+      class="toplist">
+      <ul>
+        <li
+          v-for="item in topList"
+          :key="item.id"
+          class="item">
+          <div class="icon">
+            <img v-lazy="item.picUrl" width="100" height="100">
+          </div>
+          <ul class="songlist">
+            <li
+              v-for="(item, index) in item.songList"
+              :key="index" 
+              class="song">
+              <span>{{index + 1}}</span>
+              <span>{{item.songname}}-{{item.singername}}</span>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <div v-show="!topList.length" class="load-container">
+        <loading></loading>
+      </div>
+    </scroll>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import { getTopList } from '@/api/rank'
+import { CODE } from '@/api/config'
+import Scroll from '@/base/scroll/scroll'
+import Loading from '@/base/loading/loading'
+import { playListMixin } from '@/common/js/mixin'
 export default {
-
+  mixins: [playListMixin],
+  created () {
+    this._getTopList()
+  },
+  data () {
+    return {
+      topList: []
+    }
+  },
+  methods: {
+    handlePlaylist (playingList) {
+      const bottom = playingList.length > 0 ? '60px' : ''
+      this.$refs.rank.style.bottom = bottom
+      this.$refs.topList.refresh()
+    },
+    _getTopList () {
+      getTopList()
+        .then(res => {
+          if (res.code === CODE) {
+            this.topList = res.data.topList
+          }
+        })
+    }
+  },
+  components: {
+    Scroll,
+    Loading
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
+  @import "~@/common/stylus/variable"
+  @import "~@/common/stylus/mixin"
 
-       
+  .rank
+    position fixed
+    width 100%
+    top 88px
+    bottom 0
+    .toplist
+      height 100%
+      overflow hidden
+      .item
+        display flex
+        margin 0 20px
+        padding-top 20px
+        height 100px
+        &last-child
+          padding-bottom 20px
+        .icon
+          flex 0 0 100px
+          width 100px
+          height 100px
+        .songlist
+          flex 1
+          display flex
+          flex-direction column
+          justify-content center
+          padding 0 20px
+          height 100px
+          overflow hidden
+          background $color-highlight-background
+          color $color-theme
+          font-size $font-size-small
+          .song
+            no-wrap()
+            line-height 26px
+      .loading-container
+        position absolute
+        width 100%
+        top 50%
+        transform translateY(-50%)
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Recommend from '@/components/recommend/recommend'
 import Singer from '@/components/Singer/Singer'
-// import Rank from '@/components/rank/rank'
+import Rank from '@/components/rank/rank'
 // import Search from '@/components/search/search'
 import SingerDetail from '@/components/singer-detail/singer-detail'
 import Disc from '@/components/disc/disc'
@@ -35,6 +35,16 @@ export default new Router({
           component: SingerDetail
         }
       ]
+    },
+    {
+      path: '/rank',
+      component: Rank
+      // children: [
+      //   {
+      //     path: ':id',
+      //     component: TopList
+      //   }
+      // ]
     }
   ]
 })
