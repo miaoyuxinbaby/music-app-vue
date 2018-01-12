bed84b68c228fd0b5fa31aded1d493af031c1831
miaoyuxin <1151815317@qq.com>
2018-01-12 21:02:35 +0800

    because data interface has problem. Repaired it

--- src/api/song.js
+++ src/api/song.js
@@ -1,5 +1,7 @@
-import { commonParams } from './config'
+import { commonParams, options } from './config'
 import axios from 'axios'
+import jsonp from '@/common/js/jsonp'
+import { getUid } from '@/common/js/util.js'
 
 export function getLyric (mid) {
   const url = '/api/lyric'
@@ -23,4 +25,24 @@ export function getLyric (mid) {
             // 即getLyric返回的是一个promise对象
             return Promise.resolve(res.data)
           })
+}
+
+export function getVKey (songmid, filename) {
+  const url = 'https://c.y.qq.com/base/fcgi-bin/fcg_music_express_mobile3.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    cid: 205361747,
+    format: 'json',
+    platform: 'yqq',
+    hostUin: 0,
+    needNewCode: 0,
+    uin: 0,
+    songmid,
+    filename,
+    guid: getUid()
+  })
+
+  return jsonp(url, data, Object.assign({}, options, {
+    param: 'callback'
+  }))
 }
--- src/common/js/song.js
+++ src/common/js/song.js
@@ -1,7 +1,9 @@
-import { getLyric } from '@/api/song'
+import { getLyric, getVKey } from '@/api/song'
 import { CODE } from '@/api/config'
 import { Base64 } from 'js-base64'
+import { getUid } from '@/common/js/util.js'
 
+let urlMap = {}
 export default class Song {
   constructor ({id, mid, singer, name, album, duration, image, url}) {
     this.id = id
@@ -11,7 +13,13 @@ export default class Song {
     this.album = album
     this.duration = duration
     this.image = image
-    this.url = url
+    this.filename = `C400${this.mid}.m4a`
+    // 确保一首歌曲的 id 只对应一个 url
+    if (urlMap[this.id]) {
+      this.url = urlMap[this.id]
+    } else {
+      this._genUrl()
+    }
   }
 
   _getLyric () {
@@ -26,6 +34,18 @@ export default class Song {
             reject(new Error('no lyric'))
           }
         })
+    })
+  }
+  _genUrl () {
+    if (this.url) {
+      return
+    }
+    getVKey(this.mid, this.filename).then((res) => {
+      if (res.code === CODE) {
+        const vkey = res.data.items[0].vkey
+        this.url = `http://dl.stream.qqmusic.qq.com/${this.filename}?vkey=${vkey}&guid=${getUid()}&uin=0&fromtag=66`
+        urlMap[this.id] = this.url
+      }
     })
   }
 }
